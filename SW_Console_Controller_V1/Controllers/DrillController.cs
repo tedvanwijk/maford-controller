f9fe67fdce1c9e73dd2b3a18112a9de6cc85da65
tedvanwijk <tedvanwijk@gmail.com>
2024-09-28 20:49:11 +0200

    Removed accidental import

--- SW_Console_Controller_V1/Controllers/DrillController.cs
+++ SW_Console_Controller_V1/Controllers/DrillController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Media3D;
 
 namespace SW_Console_Controller_V1.Controllers
 {

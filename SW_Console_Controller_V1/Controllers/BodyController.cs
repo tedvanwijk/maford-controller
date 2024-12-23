0922e004212135f9738e364807e9be6980c8b077
tedvanwijk <tedvanwijk@gmail.com>
2024-12-23 16:16:11 +0100

    Added helix angle to equations

--- SW_Console_Controller_V1/Controllers/BodyController.cs
+++ SW_Console_Controller_V1/Controllers/BodyController.cs
@@ -27,6 +27,7 @@ namespace SW_Console_Controller_V1.Controllers
             GeneratedProperties.HelixPitch = Math.PI * decimal.ToDouble(Properties.ToolDiameter) / Math.Tan(Properties.HelixAngle * Math.PI / 180f);
             EquationController.SetEquation("FluteHelixPitch", GeneratedProperties.HelixPitch);
             EquationController.SetEquation("FluteCount", Properties.FluteCount);
+            EquationController.SetEquation("FluteHelixAngle", Properties.HelixAngle);
 
             // TODO: check if this should happen for all tool types
             CenterController centerController = new CenterController(Properties, GeneratedProperties, SwModel, EquationManager);
